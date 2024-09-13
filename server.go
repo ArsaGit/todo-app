e5604fbe052fd1961f2f611b290964ea31953850
Artyom Arsalanov <artyom.arsalanov@gmail.com>
2024-09-13 18:53:53 +0500

    refactor: вынес получение задач в функцию

--- server.go
+++ server.go
@@ -9,11 +9,19 @@ import (
 func TodoServer(w http.ResponseWriter, r *http.Request) {
 	id := strings.TrimPrefix(r.URL.Path, "/todos/")
 
+	todo := GetTodo(id)
+
+	fmt.Fprint(w, todo)
+}
+
+func GetTodo(id string) string {
 	if id == "1" {
-		fmt.Fprintf(w, `{"title": "Название задачи", "description": "Описание задачи в деталях"}`)
+		return `{"title": "Название задачи", "description": "Описание задачи в деталях"}`
 	}
 
 	if id == "2" {
-		fmt.Fprintf(w, `{"title": "Название задачи с id2", "description": "Описание задачи в деталях todo2"}`)
+		return `{"title": "Название задачи с id2", "description": "Описание задачи в деталях todo2"}`
 	}
+
+	return ""
 }
