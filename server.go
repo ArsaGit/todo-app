97076467c6c1a2a6a4ddac67d3d416479ebf6277
Artyom Arsalanov <artyom.arsalanov@gmail.com>
2024-09-15 19:18:20 +0500

    feat: создал тип Todo, изменил тесты

--- server.go
+++ server.go
@@ -1,27 +1,32 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json"
 	"net/http"
 	"strings"
 )
+
+type Todo struct {
+	Id          string
+	Title       string
+	Description string
+}
 
 func TodoServer(w http.ResponseWriter, r *http.Request) {
 	id := strings.TrimPrefix(r.URL.Path, "/todos/")
 
 	todo := GetTodo(id)
-
-	fmt.Fprint(w, todo)
+	json.NewEncoder(w).Encode(todo)
 }
 
-func GetTodo(id string) string {
+func GetTodo(id string) Todo {
 	if id == "1" {
-		return `{"title": "Название задачи", "description": "Описание задачи в деталях"}`
+		return Todo{"1", "Название задачи", "Описание задачи в деталях"}
 	}
 
 	if id == "2" {
-		return `{"title": "Название задачи с id2", "description": "Описание задачи в деталях todo2"}`
+		return Todo{"2", "Название задачи с id2", "Описание задачи в деталях todo2"}
 	}
 
-	return ""
+	return Todo{}
 }
--- server_test.go
+++ server_test.go
@@ -1,37 +1,59 @@
 package main
 
 import (
+	"encoding/json"
 	"net/http"
 	"net/http/httptest"
+	"reflect"
 	"testing"
 )
 
 func TestGETTodos(t *testing.T) {
 	t.Run("возвращает todo c id=1", func(t *testing.T) {
+		wantedTodo := Todo{"1", "Название задачи", "Описание задачи в деталях"}
+
 		request, _ := http.NewRequest(http.MethodGet, "/todos/1", nil)
 		response := httptest.NewRecorder()
 
 		TodoServer(response, request)
 
-		got := response.Body.String()
-		want := `{"title": "Название задачи", "description": "Описание задачи в деталях"}`
+		var got Todo
+		err := json.NewDecoder(response.Body).Decode(&got)
 
-		if got != want {
-			t.Errorf("got %q want %q", got, want)
+		if err != nil {
+			t.Fatalf("Ошибка при парсинге ответа в Todo, %s", err)
+		}
+
+		if response.Code != http.StatusOK {
+			t.Fatalf("got %d, want %d", response.Code, http.StatusOK)
+		}
+
+		if !reflect.DeepEqual(got, wantedTodo) {
+			t.Errorf("got %v, want %v", got, wantedTodo)
 		}
 	})
 
 	t.Run("возвращает todo c id=2", func(t *testing.T) {
+		wantedTodo := Todo{"2", "Название задачи с id2", "Описание задачи в деталях todo2"}
+
 		request, _ := http.NewRequest(http.MethodGet, "/todos/2", nil)
 		response := httptest.NewRecorder()
 
 		TodoServer(response, request)
 
-		got := response.Body.String()
-		want := `{"title": "Название задачи с id2", "description": "Описание задачи в деталях todo2"}`
+		var got Todo
+		err := json.NewDecoder(response.Body).Decode(&got)
 
-		if got != want {
-			t.Errorf("got %q want %q", got, want)
+		if err != nil {
+			t.Fatalf("Ошибка при парсинге ответа в Todo, %s", err)
+		}
+
+		if response.Code != http.StatusOK {
+			t.Fatalf("got %d, want %d", response.Code, http.StatusOK)
+		}
+
+		if !reflect.DeepEqual(got, wantedTodo) {
+			t.Errorf("got %v, want %v", got, wantedTodo)
 		}
 	})
 }
